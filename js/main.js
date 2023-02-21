6e1a0805afc2d92d28cbf2ad34c35d89f4775f5e
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-21 15:45:40 +0100

    age discount

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// SE il passeggero è minorenne, applico uno sconto del 20% (prezzo del biglietto / 100 * 80);
-// SE il passeggero è over 65, applico invece uno sconto del 40% (prezzo del biglietto / 100 * 60);
 // Per fornire un output in forma umana, il numero ricavato dovrà contenere un massimo di due decimali che indicheranno i centesimi da pagare per il biglietto.
 
 'use strict';
@@ -15,4 +13,12 @@ passengerDistance = Number(passengerDistance);
 passengerAge = Number(passengerAge);
 
 // A questo punto, calcolo il prezzo base del biglietto (0.21€ per km percorso);
-const ticketPrice = 0.21 * passengerDistance;
+let ticketPrice = 0.21 * passengerDistance;
+
+// SE il passeggero è minorenne, applico uno sconto del 20% (prezzo del biglietto / 100 * 80);
+// SE il passeggero è over 65, applico invece uno sconto del 40% (prezzo del biglietto / 100 * 60);
+if (passengerAge < 18) {
+  ticketPrice = ticketPrice / 100 * 80;
+} else if (passengerAge >= 65) {
+  ticketPrice = ticketPrice / 100 * 60;
+}
